495023ccf9715ef0c98e00ebcf79eef546666171
xieby1 <xieby1@outlook.com>
2026-03-04 16:34:32 +0800

    tests: update check_regex_order.py

--- default.nix
+++ default.nix
@@ -7,8 +7,8 @@ let npinsed = import ./npins; in {
     pkgs.npins
     # use check_regex_order.py from xieby1/nestoc
     (let src = pkgs.fetchurl {
-      url = "https://github.com/xieby1/nestoc/raw/afbd6a39c46b2ab26c808ce34b3bb8ce80941442/doc/check_regex_order.py";
-      sha256 = "0gxyj3di9n1w5hd7pr07ad6dxn3xn6q46bi3imv84isvsvbbhpci";
+      url = "https://github.com/xieby1/nestoc/raw/b893af13e082caae5073c16605625f4cb86e6634/doc/check_regex_order.py";
+      sha256 = "1kf7p3yba100i3ir7zs2cgpivrr2369mkckcpas5n8z64pxgf2jh";
     }; in pkgs.writeScriptBin src.name "${pkgs.python3}/bin/python3 ${src} $@")
     pkgs.poppler-utils
   ];
